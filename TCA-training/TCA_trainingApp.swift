8289d061ea9ff0fa5f68b13f058aad7f9ffc2818
Fernando Putallaz <ferhedio@gmail.com>
2026-07-17 13:54:49 -0300

    Initial Commit

---
+++ TCA-training/TCA_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_trainingApp.swift
+//  TCA-training
+//
+//  Created by Fernando Putallaz on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TCA_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA-trainingTests/TCA_trainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_trainingTests.swift
+//  TCA-trainingTests
+//
+//  Created by Fernando Putallaz on 17/07/2026.
+//
+
+import Testing
+@testable import TCA_training
+
+struct TCA_trainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
